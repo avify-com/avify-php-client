e16291d9078ebe6e658bdf85d6d159022732c3ba
Juan Prendas <jprendas@bananacode.co>
2021-12-01 21:04:43 -0600

    Autoload and namespace added

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-require '/src/Avify.php'
+<?php
+
+use App\Avify;
+
+require_once realpath("vendor/autoload.php");
+
+$card = new Avify('test', 'test', 'test', 'test', 'test', 'test');
+var_dump($card);
--- src/Avify.php
+++ src/Avify.php
@@ -1,6 +1,6 @@
 <?php
-
-require '../vendor/autoload.php';
+declare(strict_types = 1);
+namespace App;
 
 Class Avify {
 
@@ -10,48 +10,50 @@ Class Avify {
     public $mode;
     public $public_key;
 
-    public function __construct($prod_base_url, $sadbox_baseUrl, $base_url, $mode, $public_key){
+    public function __construct($prod_base_url, $sandbox_base_url, $base_url, $mode, $public_key){
         $this->prod_base_url = $prod_base_url;
-        $this->sadbox_base_url = $sadbox_base_url;
+        $this->sandbox_base_url = $sandbox_base_url;
         $this->base_url = $base_url;
         $this->mode = $mode;
         $this->public_key = $public_key;
     }
 
-    function set_prod_base_url($prod_base_url){
+    function set_prod_base_url(string $prod_base_url){
         $this->prod_base_url = $prod_base_url;
     }
     function get_prod_base_url(){
         return $this->prod_base_url;
     }
 
-    function set_sandbox_base_url($sandbox_base_url){
+    function set_sandbox_base_url(string $sandbox_base_url){
         $this->sandbox_base_url = $sandbox_base_url;
     }
     function get_sadbox_baseUrl(){
         return $this->sadbox_baseUrl;
     }
 
-    function set_base_url($base_url){
+    function set_base_url(string $base_url){
         $this->base_url = $base_url;
     }
     function get_base_url(){
         return $this->base_url;
     }
 
-    function set_mode($mode){
+    function set_mode(string $mode){
         $this->mode = $mode;
     }
     function get_mode(){
         return $this->mode;
     }
     
-    function set_public_key($public_key){
+    function set_public_key(string $public_key){
         $this->public_key = $public_key;
     }
     function get_public_key(){
         return $this->public_key;
     }
+
+    
 }
 
 ?>
