3b9b3d635b83b69070c4412d06ac08469378b250
Juan Prendas <jprendas@bananacode.co>
2021-12-06 12:30:42 -0600

    encrypt function added

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ use App\Avify;
 
 require_once realpath("vendor/autoload.php");
 
-$card = new Avify('111', 'sd', 'test', 'test', 'test');
+$test = new Avify('111', 'sd', 'test', 'test', 'test');
 
-echo $card->get_key('sandbox', 'v1');
+$key = $test->get_key('sandbox', 'v1');
+$arr = json_encode(array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5));
+echo $test->encrypt($key, $arr) ;
--- src/Avify.php
+++ src/Avify.php
@@ -74,8 +74,15 @@ Class Avify {
             print "Error: " . curl_error($curl);
             exit();
         }
-        return json_encode($key);
+        return $key;
         curl_close($curl);
+    }
+
+    public function encrypt(string $key, string $data){
+        $encryption_key = base64_decode($key);
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+        $encrypted = openssl_encrypt($data, 'aes-256-cbc', $encryption_key, 0, $iv);
+        return base64_encode($encrypted .'::'.$iv);
     }
 
     
